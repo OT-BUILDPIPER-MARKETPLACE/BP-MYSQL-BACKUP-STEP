f3c51bbb72bbedaf30d32298e0e3b4503c978cde
Priyanshi Chauhan <priyanshi.chauhan@opstree.com>
2023-02-10 01:26:23 +0530

    initial commit

---
+++ build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+source BP-BASE-SHELL-STEPS/functions.sh
+source BP-BASE-SHELL-STEPS/log-functions.sh
+source BP-BASE-SHELL-STEPS/str-functions.sh
+source BP-BASE-SHELL-STEPS/file-functions.sh
+source BP-BASE-SHELL-STEPS/aws-functions.sh
+
+logInfoMessage "I'll take a backup of the buildpiper mysql database"
+logInfoMessage "Received below arguments"
+logInfoMessage "Database: $DATABASE"
+logInfoMessage "Database backup directory: $DATABASE_BACK_DIR"
+
+DATE=$(date +"%d-%m-%-Y")
+
+if [ -f "key.pem" ]; then
+   true
+else
+   cat "$PRIVATE_KEY" > key.pem && chmod 400 key.pem
+fi
+
+SERVER="ssh $USERNAME@$IP_ADDRESS -i key.pem -p $SSH_PORT -o stricthostkeychecking=no"
+
+if [ ! -d "$HOME/$DATABASE_BACK_DIR" ]; then
+   echo "Creating directory for mysql database backup: $DATABASE_BACK_DIR"
+   mkdir $HOME/$DATABASE_BACK_DIR 
+else
+   true
+fi
+
+DB_CONTAINER=$( $SERVER "docker ps" | awk '{print $NF}' | grep 'db')
+
+if [ "$DB_CONTAINER" == "db" ]; then
+   $SERVER docker exec -i db mysqldump -u$DB_USER -p$DB_PASSWORD $DATABASE > $HOME/$DATABASE_BACK_DIR/sql_dump_$DATE.sql
+   logInfoMessage "Congratulations Buildpiper database backup has been successfully taken!!!"
+   generateOutput $ACTIVITY_SUB_TASK_CODE true "Congratulations Buildpiper database backup has been successfully taken!!!"
+else
+  if [ $VALIDATION_FAILURE_ACTION == "FAILURE" ]
+  then
+      logErrorMessage "db: Container not found"
+      logErrorMessage "Failed to take buildpiper database backup please check!!!"
+      generateOutput $ACTIVITY_SUB_TASK_CODE false "Failed to take buildpiper database backup please check!!!"
+      exit 1
+  else
+      logWarningMessage "db: Container not found"
+      logWarningMessage "Failed to take buildpiper database backup please check!!!"
+      generateOutput $ACTIVITY_SUB_TASK_CODE true "Failed to take buildpiper database backup please check!!!"
+  fi
+fi
+
+
