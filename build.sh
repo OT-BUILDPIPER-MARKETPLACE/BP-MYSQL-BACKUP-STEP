9acc5b744c2b4a5fda849203a13689e2175c1031
Priyanshi Chauhan <107524071+Cpriyanshi77@users.noreply.github.com>
2023-04-03 20:46:17 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,13 +18,13 @@ CURRENT_DATE=$(date '+%e_%b_%Y-%I:%M')
 if [ -f "key.pem" ]; then
    true
 else
-   echo -e "$PRIVATE_KEY_FILE" > key.pem
+   echo -e "$SSH_PRIVATE_KEY_FILE" > key.pem
    sed -i 's/ \+/\'$'\n/g' key.pem && sed -i '1,4d' key.pem 
    sed -i '1 i -----BEGIN RSA PRIVATE KEY-----' key.pem && sed -i '27,30d' key.pem 
    echo "-----END RSA PRIVATE KEY-----" >> key.pem && chmod 400 key.pem
 fi
 
-SERVER="ssh $USERNAME@$IP_ADDRESS -i key.pem -p $SSH_PORT -o stricthostkeychecking=no"
+SERVER="ssh $SSH_USERNAME@$SSH_IP_ADDRESS -i key.pem -p $SSH_PORT -o stricthostkeychecking=no"
 
 if $SERVER [ -d $DATABASE_BACKUP_DIR ]; then
    true
